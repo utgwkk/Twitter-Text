e2248e14ffa45062323acf52d8f2d43700c69376
utgwkk <utagawakiki@gmail.com>
2020-10-21 10:58:18 +0000

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Twitter::Text - It's new $module
+
+# SYNOPSIS
+
+    use Twitter::Text;
+
+# DESCRIPTION
+
+Twitter::Text is ...
+
+# LICENSE
+
+Copyright (C) utgwkk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+utgwkk <utagawakiki@gmail.com>
---
+++ lib/Twitter/Text.pm
@@ -0,0 +1,39 @@
+package Twitter::Text;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Twitter::Text - It's new $module
+
+=head1 SYNOPSIS
+
+    use Twitter::Text;
+
+=head1 DESCRIPTION
+
+Twitter::Text is ...
+
+=head1 LICENSE
+
+Copyright (C) utgwkk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+utgwkk E<lt>utagawakiki@gmail.comE<gt>
+
+=cut
+
