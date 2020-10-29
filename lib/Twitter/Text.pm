8944272e4d635b8bd5288731a5db2d05715ca085
utgwkk <utagawakiki@gmail.com>
2020-10-29 12:22:23 +0900

    pod item and description

--- lib/Twitter/Text.pm
+++ lib/Twitter/Text.pm
@@ -508,15 +508,25 @@ The C<parse_tweet> function takes a C<$text> string and optional C<\%options> pa
 
 =over 4
 
-=item C<weighted_length>: the overall length of the tweet with code points weighted per the ranges defined in the configuration file.
+=item C<weighted_length>
 
-=item C<permillage>: indicates the proportion (per thousand) of the weighted length in comparison to the max weighted length. A value > 1000 indicates input text that is longer than the allowable maximum.
+The overall length of the tweet with code points weighted per the ranges defined in the configuration file.
 
-=item C<valid>: indicates if input text length corresponds to a valid result.
+=item C<permillage>
 
-=item C<display_range_start>, C<display_range_end>: An array reference of two unicode code point indices identifying the inclusive start and exclusive end of the displayable content of the Tweet.
+Indicates the proportion (per thousand) of the weighted length in comparison to the max weighted length. A value > 1000 indicates input text that is longer than the allowable maximum.
 
-=item C<vaildRangeStart>, C<valid_range_end>: An array reference of two unicode code point indices identifying the inclusive start and exclusive end of the valid content of the Tweet.
+=item C<valid>
+
+Indicates if input text length corresponds to a valid result.
+
+=item C<display_range_start>, C<display_range_end>
+
+An array reference of two unicode code point indices identifying the inclusive start and exclusive end of the displayable content of the Tweet.
+
+=item C<vaildRangeStart>, C<valid_range_end>
+
+An array reference of two unicode code point indices identifying the inclusive start and exclusive end of the valid content of the Tweet.
 
 =back
 
